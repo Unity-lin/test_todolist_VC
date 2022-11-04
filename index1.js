e690efb1c1bc029f879c556374d347d9b4687ec8
ichaehun <ichaehunn@gmail.com>
2022-11-05 01:46:20 +0900

    근데 방법을 약간 더찾음

--- index1.js
+++ index1.js
@@ -21,12 +21,21 @@ todoButtons[2].addEventListener("click", deleteHandler);
    function submitTodoHandler(event) {
     if(event.target.id == "btn1"){
         const h2 = document.createElement('h2');
-        const h2Text = document.createTextNode(todoInputs[0].value);
+        const h2Text = document.createTextNode(todoInputs.value);
         const button1 = document.createElement('button');
         const button1Text = document.createTextNode('Done');
         const button2 = document.createElement('button');
         const button2Text = document.createTextNode('Delete');
 
+        button1.setAttribute('class', 'todo-button');
+        button1.setAttribute('id', 'btn2');
+        button1.setAttribute('value', '1');
+        button1.setAttribute('type', 'button');
+        button2.setAttribute('class', 'todo-button');
+        button2.setAttribute('id', 'btn3');
+        button2.setAttribute('value', '2');
+        button2.setAttribute('type', 'button');
+        h2.setAttribute('id' , 'h2style');
         todoResult.appendChild(h2);
         todoResult.appendChild(button1);
         todoResult.appendChild(button2);
@@ -43,8 +52,8 @@ todoButtons[2].addEventListener("click", deleteHandler);
    }
    function completeHandler(event) {
     if(event.target.id == "btn2"){          //Done 버튼 아이디
-        if(event.target.id == "H2S"){       //h2 아이디
-                                            //todoResult 안에있는 h2글씨를 text-decoration : line-through을 해야하는데 막힘
+        if(event.target.id == "h2style"){       //h2 아이디
+            h2.setAttribute('text-decoration', 'line-through');                          
             
         }
     }   
@@ -67,20 +76,5 @@ todoButtons[2].addEventListener("click", deleteHandler);
    }
 
 
-  /* var itemList = document.querySelector('.itemList');
 
-var li = document.createElement('li');
-var input = document.createElement('input');
-var label = document.createElement('label');
-var labelText = document.createTextNode('이벤트 위임 학습');
-
-input.setAttribute('type', 'checkbox');
-input.setAttribute('id', 'item3');
-label.setAttribute('for', 'item3');
-label.appendChild(labelText);
-li.appendChild(input);
-li.appendChild(label);
-itemList.appendChild(li);
-
-*/
 
